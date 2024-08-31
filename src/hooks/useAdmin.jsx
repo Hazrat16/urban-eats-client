e883c049c64130cad7f4fa460a06a2313425e64c
hazrat <hazrat17016@gmail.com>
2024-08-31 11:58:23 +0600

    lint issue solved

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,10 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
-import useAuth from './useAuth';
-import useAxiosSecure from './useAxiosSecure';
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
   const { user } = useAuth();
-  const axiosSecure =                                  useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
   const { data: isAdmin, isPending: isAdminLoading } = useQuery({
     queryKey: [user?.email, "isAdmin"],
     queryFn: async () => {
