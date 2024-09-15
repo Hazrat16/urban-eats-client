8cda0cd80e662d0a49ad375793d77a12147cc61b
hazrat <hazrat17016@gmail.com>
2024-09-15 13:10:35 +0600

    dockerize the client side code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,7 +28,7 @@ module.exports = {
     "import/resolver": {
       node: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
-        moduleDirectory: ["node_modules", "src/"],
+        moduleDirectory: ["node_modules", "src/", "node_modules/swiper"],
       },
     },
     react: {
